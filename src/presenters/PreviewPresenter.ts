cc06b65685a2c5c177659b89d091f6a094f9055f
Kagura FrontEnd <kuzinaflkuzina@gmail.com>
2025-06-22 03:02:46 +0700

    modalka escape added

--- src/presenters/PreviewPresenter.ts
+++ src/presenters/PreviewPresenter.ts
@@ -2,15 +2,26 @@ import { EventEmitter } from '../components/base/events';
 import { IProductItem } from '../types';
 import { IView } from '../interfaces/IView';
 
+interface IPreviewView extends IView<IProductItem> {
+  close(): void;
+}
+
 export class PreviewPresenter {
   constructor(
     private events: EventEmitter,
-    private view: IView<IProductItem>
+    private view: IPreviewView
   ) {}
 
   public init(): void {
     this.events.on<IProductItem>('preview:changed', (item) => {
       this.view.render(item);
+    });
+
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.classList.contains('modal__close')) {
+        this.view.close();
+      }
     });
   }
 }
