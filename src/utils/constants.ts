5ebe2b2d050ea3847f266a49e7ba755b7465bd07
Rents <Rents@users.noreply.github.com>
2025-06-25 01:04:25 +0300

    gitignore temp IDE files (ends with "~"), changed webpack port (8182), adds DEFAULT_IMAGE setting to .env, constants.ts and webpack.config.js

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek/`;
-export const DEFAULT_IMAGE = "src/images/trash-2.svg";
+export const DEFAULT_IMAGE = `${process.env.DEFAULT_IMAGE}`;
+
 
 export const settings = {
 
